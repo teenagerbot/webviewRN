9eb4cbda81bd01e115c818ca3b4559a802ad42b8
developer <91476715+teenagerbot@users.noreply.github.com>
2023-11-17 20:20:46 +0200

    a

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <WebView source={{
+        uri: 'https://www.google.com'
+      }}
+      style={{ flex: 1 }}
+      javaScriptEnabled={true}
+  domStorageEnabled={true}
+  originWhitelist={['*']}
+  mixedContentMode="always"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
